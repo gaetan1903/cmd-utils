2d92622f3a8a80905ac84ab9c4a9375a98200e0c
Gaetan Jonathan <gaetan.jonathan.bakary@esti.mg>
2021-01-19 15:54:26 +0100

    Fix Bug & Decore Output & performance

--- kill-grep.py
+++ kill-grep.py
@@ -1,11 +1,13 @@
-#!/usr/bin/python 
- 
-import os, re
+import os, re, sys
 
-v = os.popen("ps -eo pid,command |grep " + sys.argv[]).read().split("\n")
+v = os.popen("ps -eo pid,command |grep -v grep|grep " + sys.argv[1]).read().split("\n")
 
+print len(v)-1, "processus trouvee"
+print
+print
 for u in v:
     uu = re.findall(r"[0-9]{3,}", u)
-    if len(uu) > 0:
-        os.system("kill -9 " + uu[0])
-        print("processus :" + u + " x")
+    if len(uu) > 0 and str(os.getpid()) != uu[0]:
+        if os.system("kill -9 " + uu[0]+ " > /dev/null") == 0:
+            print "processus :" + u + " arreter"
+            print
