b1f5f9a650bd85835a131fdf2fcae0438e21e559
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2021-01-19 20:58:12 +0300

    Update kill-grep.py
    migrate to python3

--- kill-grep.py
+++ kill-grep.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import os, re, sys
 
 v = os.popen("ps -eo pid,command |grep -v grep|grep " + sys.argv[1]).read().split("\n")
 
-print len(v)-1, "processus trouvee"
-print
-print
+print(len(v)-1, "processus trouvee")
+print()
+print()
 for u in v:
     uu = re.findall(r"[0-9]{3,}", u)
     if len(uu) > 0 and str(os.getpid()) != uu[0]:
         if os.system("kill -9 " + uu[0]+ " > /dev/null") == 0:
-            print "processus :" + u + " arreter"
-            print
+            print("processus :" + u + " arreter")
+            print()
